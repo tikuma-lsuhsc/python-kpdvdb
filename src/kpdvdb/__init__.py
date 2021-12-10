d9e6e9ec5bcff4e30dfae61c7d49b229431eeae0
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2021-12-10 11:57:18 -0600

    added get_diagnoses(), get_locations(), get_natlangs(), get_origins(), get_sexes(), query()

--- src/kpdvdb/__init__.py
+++ src/kpdvdb/__init__.py
@@ -2,3 +2,262 @@
 
 __version__ = "0.0.0"
 
+import pandas as pd
+from os import path
+import numpy as np
+from glob import glob as _glob
+import re, operator
+
+# global variables
+_dir = None  # database dir
+_df = None  # main database table
+_df_dx = None  # patient diagnosis table
+_dx = None  # patient diagnoses series
+
+
+def load_db(dbdir):
+    """load disordered voice database
+
+    :param dbdir: path to the cdrom drive or the directory hosting a copy of the database
+    :type dbdir: str
+
+    * This function must be called at the beginning of each Python session
+    * Database is loaded from the text file found at: <dbdir>/EXCEL50/TEXT/KAYCDALL.TXT
+    * Only entries with NSP files are included
+    * PAT_ID of the entries without PAT_ID field uses the "FILE VOWEL 'AH'" field value
+      as the PAT_ID value
+
+    """
+
+    global _dir, _df, _df_dx, _dx
+
+    if _dir == dbdir:
+        return
+
+    ra_files = operator.iconcat(
+        *(
+            _glob(path.join(dbdir, vtype, "RAINBOW", "*.NSP"))
+            for vtype in ("NORM", "PATHOL")
+        )
+    )
+
+    df = pd.read_table(
+        path.join(dbdir, "EXCEL50", "TEXT", "KAYCDALL.TXT"),
+        skipinitialspace=True,
+        dtype={
+            # fmt: off
+            **{col: 'Int32' for col in ["AGE", "#", "NVB", "NSH", "NUV", "SEG", "PER"]},
+            **{col: "string" for col in ["PAT_ID", "FILE VOWEL 'AH'"]},
+            **{col: "category" for col in ["SEX", "DIAGNOSIS", "LOCATION", "NATLANG", "ORIGIN"]},
+            **{col: float for col in ["Fo", "To", "Fhi", "Flo", "STD", "PFR", "Fftr", "Fatr", "Tsam", "Jita", 
+                                    "Jitt", "RAP", "PQ", "sPPQ", "vFo", "ShdB", "Shim", "APQ", "sAPQ", 
+                                    "vAm", "NHR", "VTI", "SPI", "FTRI", "ATRI", "DVB", "DSH", "DUV"]},
+            "SMOKE": 'boolean'
+            # fmt: on
+        },
+        parse_dates=["VISITDATE"],
+        true_values=["Y"],
+        false_values=["N"],
+    )
+
+    # add PAT_ID if missing (use unique ID based on the file name)
+    tf = pd.isna(df["PAT_ID"])
+    for row in np.where(tf)[0]:
+        # id = df.at[row, "FILE VOWEL 'AH'"][:3]
+        # ids = df["PAT_ID"][df["PAT_ID"].str.startswith(id, na=False)].tolist()
+        # if len(ids):
+        #     for i in range(1000):
+        #         id_ = f"{id}{i:03}"
+        #         if id_ not in ids:
+        #             break
+        # else:
+        #     id_ = f"{id}000"
+        # df.at[row, "PAT_ID"] = id_
+        df.at[row, "PAT_ID"] = df.at[row, "FILE VOWEL 'AH'"]
+
+    # split dx
+    df_dx = df[["PAT_ID", "VISITDATE", "DIAGNOSIS"]]
+    df.drop(columns=["#", "DIAGNOSIS"], inplace=True)
+    df.drop_duplicates(subset=["PAT_ID", "VISITDATE"], inplace=True)
+    df.reset_index(inplace=True)
+
+    # assign for rainbow
+    ra_names = set((path.basename(f) for f in ra_files))
+
+    # remove entries without PAT_ID or FILE VOWEL 'AH'
+    n = df.shape[0]
+    rnames = [None] * n
+    isnorms = np.empty(n, bool)
+    tf = np.zeros(n, bool)
+    for row in range(n):
+        aname = df.at[row, "FILE VOWEL 'AH'"]
+        if pd.isna(aname):
+            id = df.at[row, "PAT_ID"][:3]
+            rname = f"{id}1NRL.NSP"
+            isnorms[row] = isnorm = rname in ra_names
+            if not isnorm:
+                day = df.at[row, "VISITDATE"].day
+                rname = f"{id}{day:02}R.NSP"
+        else:
+            isnorms[row] = isnorm = aname.endswith("NAL.NSP")
+            rname = (
+                re.sub(r"NAL\.NSP$", "NRL.NSP", aname)
+                if isnorm
+                else re.sub(r"AN\.NSP$", "R.NSP", aname)
+            )
+
+        if rname in ra_names:
+            rnames[row] = rname
+        else:
+            tf[row] = True
+
+    df.insert(3, "FILE RAINBOW", pd.Series(rnames, dtype="string"))
+    df.insert(3, "NORM", pd.Series(isnorms, dtype="boolean"))
+    df.drop(index=np.where(tf)[0], inplace=True)
+    df.reset_index(inplace=True)
+    df.drop(columns=["index", "level_0"], inplace=True)
+
+    _dir = dbdir
+    _df = df
+    _df_dx = df_dx
+    _dx = None
+
+
+def get_sexes():
+    """get unique entries of SEX field
+
+    :return: fixed list ["F", "M"]
+    :rtype: list(str)
+    """
+    return _df["SEX"].cat.categories.tolist()
+
+
+def get_locations():
+    """get unique entries of LOCATION field
+
+    :return: list of sites of disorders
+    :rtype: list(str)
+    """
+    return _df["LOCATION"].cat.categories.tolist()
+
+
+def get_natlangs():
+    """get unique entries of NATLANGS field
+
+    :return: list of subjects' native languages
+    :rtype: list(str)
+    """
+    return _df["NATLANG"].cat.categories.tolist()
+
+
+def get_origins():
+    """get unique entries of ORIGIN field
+
+    :return: list of subjects' races
+    :rtype: list(str)
+    """
+    return _df["ORIGIN"].cat.categories.tolist()
+
+
+def get_diagnoses():
+    """get unique entries of DIAGNOSIS field
+
+    :return: list of diagnoses
+    :rtype: list(str)
+    """
+    return _df_dx["DIAGNOSIS"].cat.categories.tolist()
+
+
+def _get_dx_series():
+    global _dx
+    if _dx is None:  # one-time operation
+        _dx = pd.Series(
+            [
+                _df_dx[(_df_dx["PAT_ID"] == id) & (_df_dx["VISITDATE"] == date)][
+                    "DIAGNOSIS"
+                ]
+                .dropna()
+                .tolist()
+                for id, date in zip(_df["PAT_ID"], _df["VISITDATE"])
+            ]
+        )
+    return _dx
+
+
+def query(subset=None, **filters):
+    """query database
+
+    :param subset: database columns to return, defaults to None
+    :type subset: sequence of str, optional
+    :param **filters: query conditions (values) for specific per-database columns (keys)
+    :type **filters: dict
+    :return: query result
+    :rtype: pandas.DataFrame
+
+    Valid values of `subset` argument
+    ---------------------------------
+    
+    * All columns of the database specified in EXCEL50/TEXT/README.TXT Section 3.1 
+      (except for "DIAGNOSIS" and "#")
+    * "DIAGNOSES" - A list containing all the original "DIAGNOSIS" associated with the subject
+    * "MDVP" - Short-hand notation to include all the MDVP parameter measurements: from "Fo" to "PER"
+
+    Valid `filters` keyword arguments
+    ---------------------------------
+    
+    * All columns of the database specified in EXCEL50/TEXT/README.TXT Section 3.1 
+      (except for "DIAGNOSIS" and "#")
+    * "DIAGNOSES" - A list containing all the original "DIAGNOSIS" associated with the subject
+
+    Valid `filters` keyword argument values
+    ---------------------------------------
+
+    * A scalar value
+    * For numeric and date columns, 2-element sequence to define a range: [start, end)
+    * For all other columns, a sequence of allowable values
+
+    """
+
+    # work on a copy of the dataframe
+    df = _df.copy(deep=True)
+    if (subset is None or "DIAGNOSES" in subset) or "DIAGNOSES" in filters:
+        df.insert(3, "DIAGNOSES", _get_dx_series())
+
+    # apply the filters to reduce the rows
+    for fcol, fcond in filters.items():
+        try:
+            s = df[fcol]
+        except:
+            raise ValueError(f"{fcol} is not a valid column label")
+        if fcol == "DIAGNOSES":
+            fcond = set([fcond]) if isinstance(fcond, str) else set(fcond)
+            df = df[[len(fcond & set(v)) > 0 for v in s]]
+        else:
+            try:  # try range/multi-choices
+                if s.dtype.kind in "iufcM":  # numeric/date
+                    # 2-element range condition
+                    df = df[(s >= fcond[0]) & (s < fcond[1])]
+                else:  # non-numeric
+                    df = df[s.isin(fcond)]  # choice condition
+            except:
+                # look for the exact match
+                df = df[s == fcond]
+
+    # return only the selected columns
+    if subset is not None:
+        try:
+            i = subset.index("MDVP")
+            cols = df.columns
+            j = np.where(cols == "Fo")[0][0]
+            subset = [*subset[:i], *cols[j:].values, *subset[i + 1 :]]
+        except:
+            pass
+        try:
+            df = df[subset]
+        except:
+            ValueError(
+                f'At least one label in the "subset" argument is invalid: {subset}'
+            )
+
+    return df
+
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,3 +1,4 @@
+import kpdvdb
 import pandas as pd
 from os import path
 import numpy as np
@@ -5,90 +6,38 @@ from glob import glob
 import re
 
 dbdir = (
-    r"C:\Users\tikuma\OneDrive - LSUHSC\data\KayPENTAX Disordered Voice Database"
-    # r"C:\Users\Takeshi Ikuma\OneDrive - LSUHSC\data\KayPENTAX Disordered Voice Database"
+    r"C:\Users\tikum\OneDrive - LSUHSC\data\KayPENTAX Disordered Voice Database"  # excalibur
+    # r"C:\Users\tikuma\OneDrive - LSUHSC\data\KayPENTAX Disordered Voice Database" # samsung
+    # r"C:\Users\Takeshi Ikuma\OneDrive - LSUHSC\data\KayPENTAX Disordered Voice Database" # olol
 )
 
-_files = [
-    glob(path.join(dbdir, vtype, atype, "*.NSP"))
-    for vtype in ("NORM", "PATHOL")
-    for atype in ("AH", "RAINBOW")
-]
-ah_files = [*_files[0], *_files[2]]
-ra_files = [*_files[1], *_files[3]]
 
-df = pd.read_table(
-    path.join(dbdir, "EXCEL50", "TEXT", "KAYCDALL.TXT"),
-    skipinitialspace=True,
-    dtype={
-        # fmt: off
-        **{col: 'Int32' for col in ["AGE", "#", "NVB", "SH", "NUV", "SEG", "PER"]},
-        **{col: "string" for col in ["PAT_ID", "FILE VOWEL 'AH'"]},
-        **{col: "category" for col in ["SEX", "DIAGNOSIS", "LOCATION",  "SMOKE", "NATLANG", "ORIGIN"]},
-        **{col: float for col in ["Fo", "To", "Fhi", "Flo", "STD", "PFR", "Fftr", "Fatr", "Tsam", "Jita", 
-                                  "Jitt", "RAP", "PQ", "sPPQ", "vFo", "ShdB", "Shim", "APQ", "sAPQ", 
-                                  "vAm", "NHR", "VTI", "SPI", "FTRI", "ATRI", "DVB", "DSH", "DUV"]}
-        # fmt: on
-    },
-    parse_dates=["VISITDATE"],
-)
+def test_basics():
+    kpdvdb.load_db(dbdir)
+    print(kpdvdb.get_sexes())
+    print(kpdvdb.get_locations())
+    print(kpdvdb.get_natlangs())
+    print(kpdvdb.get_origins())
+    print(kpdvdb.get_diagnoses())
 
-# add PAT_ID if missing (use unique ID based on the file name)
-tf = pd.isna(df["PAT_ID"])
-for row in np.where(tf)[0]:
-    # id = df.at[row, "FILE VOWEL 'AH'"][:3]
-    # ids = df["PAT_ID"][df["PAT_ID"].str.startswith(id, na=False)].tolist()
-    # if len(ids):
-    #     for i in range(1000):
-    #         id_ = f"{id}{i:03}"
-    #         if id_ not in ids:
-    #             break
-    # else:
-    #     id_ = f"{id}000"
-    # df.at[row, "PAT_ID"] = id_
-    df.at[row, "PAT_ID"] = df.at[row, "FILE VOWEL 'AH'"]
 
-# split dx
-df_dx = df[["PAT_ID", "VISITDATE", "DIAGNOSIS"]]
-df.drop(columns=["#", "DIAGNOSIS"], inplace=True)
-df.drop_duplicates(subset=["PAT_ID", "VISITDATE"], inplace=True)
-df.reset_index(inplace=True)
+kpdvdb.load_db(dbdir)
 
-# assign for rainbow
-ra_names = set((path.basename(f) for f in ra_files))
+# df = kpdvdb.query()
+# df = kpdvdb.query(["MDVP"])
+# df = kpdvdb.query(["DIAGNOSES"])
 
-# remove entries without PAT_ID or FILE VOWEL 'AH'
-n = df.shape[0]
-rnames = [None]*n
-isnorms = np.empty(n, bool)
-tf = np.zeros(n, bool)
-for row in range(n):
-    aname = df.at[row, "FILE VOWEL 'AH'"]
-    if pd.isna(aname):
-        id = df.at[row, "PAT_ID"][:3]
-        rname = f"{id}1NRL.NSP"
-        isnorms[row] = isnorm = rname in ra_names
-        if not isnorm:
-            day = df.at[row, "VISITDATE"].day
-            rname = f"{id}{day:02}R.NSP"
-    else:
-        isnorms[row] = isnorm = aname.endswith("NAL.NSP")
-        rname = (
-            re.sub(r"NAL\.NSP$", "NRL.NSP", aname)
-            if isnorm
-            else re.sub(r"AN\.NSP$", "R.NSP", aname)
-        )
+# df = kpdvdb.query(
+#     ["NORM", "SEX", "Fo", "NATLANG", "VISITDATE"],
+#     NORM=False,
+#     SEX="M",
+#     Fo=[50, 150],
+#     NATLANG=["English", "Japanese"],
+#     VISITDATE=[np.datetime64('1993-01-01'),np.datetime64('1994-01-01')],
+# )
 
-    if rname in ra_names:
-        rnames[row] = rname
-    else:
-        tf[row] = True
-
-print(rnames)
-
-df.insert(3, "FILE RAINBOW", rnames)
-df.insert(3, "NORM", isnorms)
-df.drop(index=np.where(tf)[0], inplace=True)
-df.reset_index(inplace=True)
+df = kpdvdb.query(["DIAGNOSES"], DIAGNOSES=["normal voice", "paralysis"])
 
 print(df)
+# ["diagnoses"]
+# print(df)
